f72afe2394efc1b37e20b8ed9e6f2f3b05ca7971
rinoard <rino.ardiansyah1994@gmail.com>
2019-01-15 14:37:38 +0700

    membuat solusi PR logic baru

---
+++ PRLogic/PRLogic01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PRLogic01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
