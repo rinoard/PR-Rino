fb348202aa037b7ba10abca298b776f5991f5e7e
rinoard <rino.ardiansyah1994@gmail.com>
2019-01-22 08:38:10 +0700

    PR Logic 5 Soal PR1 & 2

---
+++ Exam/ExamHari05/PR1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ExamBase;
+
+namespace ExamHari05
+{
+    class PR1: LogicBase
+    {
+        public PR1(int n)
+        {
+            JmlhBaris = (n * n + n) / 2;
+            JmlhKolom = n*n;
+            Array2D = new string[JmlhBaris, JmlhKolom];
+            IsiArray(n);
+            FunctionBase.Cetak(Array2D);
+        }
+
+        private void IsiArray(int n)
+        {
+            int[] fibo = FunctionBase.Fibonacci(JmlhBaris * JmlhKolom);
+            for (int bangun = 0; bangun < n; bangun++)
+            {
+                int angka = 1;
+                int StartBaris = JmlhBaris -1-((bangun*bangun+bangun)/2);
+                int StartKolom = bangun*bangun;
+                int EndBaris = StartBaris -(bangun);
+                int EndKolom = StartKolom + (bangun*2);
+                for (int b = 0; b <= StartBaris ; b++)
+                {
+                    for (int k = StartKolom; k <= EndKolom; k++)
+                    {
+                        if(b+k>=StartKolom +StartBaris &&k-b<=EndKolom -StartBaris )
+                        Array2D[b, k] = fibo[angka++].ToString();                        
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ Exam/ExamHari05/PR2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ExamBase;
+
+namespace ExamHari05
+{
+    class PR2 : LogicBase
+    {
+        public PR2(int n)
+        {
+            JmlhBaris = (n * n + n) / 2;
+            JmlhKolom = n * n;
+            Array2D = new string[JmlhBaris, JmlhKolom];
+            IsiArray(n);
+            FunctionBase.Cetak(Array2D);
+        }
+
+        private void IsiArray(int n)
+        {
+            int[] fibo = FunctionBase.Fibonacci(JmlhBaris * JmlhKolom);
+            for (int bangun = 0; bangun < n; bangun++)
+            {
+                int angka = 0;
+                int StartBaris = JmlhBaris - 1 - ((bangun * bangun + bangun) / 2);
+                int StartKolom = JmlhKolom -1-( bangun * bangun);
+                int EndBaris = StartBaris - (bangun);
+                int EndKolom = StartKolom - (bangun * 2);
+                for (int b = 0; b <= StartBaris; b++)
+                {
+                    for (int k = StartKolom; k >= EndKolom; k--)
+                    {
+                        if (b + k >= EndKolom  + StartBaris && k - b <= StartKolom  - StartBaris)
+                            Array2D[b, k] = fibo[angka++].ToString();
+                    }
+                }
+
+            }
+        }
+    }
+}
